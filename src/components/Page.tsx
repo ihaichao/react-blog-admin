41ce7e625839d789ead9373036d5e626c834c611
zhangzongzheng <zzzhaichao@gmail.com>
2017-11-14 00:09:07 +0800

    update route

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,7 +4,7 @@ class Page extends React.Component {
   render() {
     return (
       <div>
-        { this.props.children }
+        {this.props.children}
       </div>
     )
   }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Router, Route, hashHistory } from 'react-router'
+import Page from '../components/Page'
 import WriteArticle from '../components/WriteArticle'
 
 export default (
 	<Router history={hashHistory}>
-		<Route path="/article/write" component={WriteArticle} />
+		<Route path="/" component={Page}>
+			<Route path="/article/write" component={WriteArticle} />
+		</Route>
 	</Router>
 )
