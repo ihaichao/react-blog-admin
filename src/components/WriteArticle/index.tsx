b29e14f97a11e9b6f5210244ec89c40b6802752e
zhangzongzheng <zzzhaichao@gmail.com>
2017-11-15 23:55:10 +0800

    add markdown editor

--- src/components/WriteArticle/index.tsx
+++ src/components/WriteArticle/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+// import ReactMde, { ReactMdeCommands } from 'react-mde'
+const ReactMde = require('react-mde')
+const { ReactMdeCommands } = require('react-mde') 
 
-class WriteArticle extends React.Component {
+interface ComponentProps {
+
+}
+
+interface ComponentState {
+	reactMdeValue: object
+}
+
+class WriteArticle extends React.Component<ComponentProps, ComponentState> {
+	constructor(props: object) {
+		super(props)
+		this.state = {
+			reactMdeValue: {
+				text: '',
+				selection: null
+			}
+		}
+	}
+
+	handleValueChange = (value: object) => {
+		this.setState({reactMdeValue: value})
+	}
+
 	render() {
 		return (
-			<div className="write-article">Write article.</div>
+			<div className="container">
+				<ReactMde
+					textAreaProps={{
+							id: 'ta1',
+							name: 'ta1',
+					}}
+					value={this.state.reactMdeValue}
+					onChange={this.handleValueChange}
+					commands={ReactMdeCommands}
+				/>
+			</div>
 		)
 	}
 }
